a0ac2a6e009453f05e2f5842464ec0e7b0c473db
agandia9 <alejandro.gandia9@gmail.com>
2018-01-30 14:23:56 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import Card from './card/card.js'
+import { Button, Row, Navbar, NavItem, Col } from 'react-materialize'
+
+class App extends Component {
+    status={
+      tasks: [{
+        id:'1',
+        title: 'Lorem?',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        date: '12/2'
+      },
+      {
+        id:'2',
+        title: 'Lorem!',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        date: '14/2'
+      }]
+    }
+  render() {
+
+    return (
+      <div className="App">
+        <Navbar brand='logo' left>
+          <NavItem href='get-started.html'>
+            <img href={logo} />
+          </NavItem>
+        </Navbar>
+        <p className="App-intro">
+        <h2>Add new todo's</h2>
+         <Row>
+            {
+              this.status.tasks.map((elem)=>{
+                return(
+                  <Card 
+                      id={elem.id}
+                      title={elem.title}
+                      description={elem.description}
+                  />
+                  )
+              })
+            }
+            <Button  floating large className='blue' waves='light' icon='add' s={2}/>
+          </Row>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/card/card.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Col, Card } from 'react-materialize'
+
+class App extends Component {
+  render() {
+    return (
+		<Col s={2} className='grid-example'>
+            <Card className='blue-grey darken-1' textClassName='white-text' title={this.props.title} actions={[<a href='#'>Delete task</a>]}>
+            	{this.props.description}
+            </Card>
+		</Col>
+    );
+  }
+}
+
+export default App;
