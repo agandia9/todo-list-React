b96468a5f36985e6f40dc9ec6371109ded61525b
agandia9 <alejandro.gandia9@gmail.com>
2018-01-30 16:59:25 +0100

    fail when delete all tasks and insert new one

--- src/components/App.js
+++ src/components/App.js
@@ -3,40 +3,65 @@ import logo from '../logo.svg';
 import './App.css';
 
 import Card from './card/card.js'
-import { Button, Row, Navbar, NavItem, Col, Modal } from 'react-materialize'
+import { Button, Row, Navbar, NavItem, Col, Modal, Icon, Input } from 'react-materialize'
 
 class App extends Component {
   constructor(props){
     super()
     this.state={
       tasks: [{
-        id:'1',
+        id:1,
         title: 'Lorem?',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
         date: '12/2'
       },
       {
-        id:'2',
+        id:2,
         title: 'Lorem!',
         description: 'Maiores fugit veniam, dolore assumenda adipisicing elit.',
         date: '14/2'
-      }]
+      }],
+      newTaskTitle:'',
+      newTaskDescription:''
     }
   }
+
+    setTitle = (e) => {
+      this.setState({
+        newTaskTitle: e.target.value
+      })
+    }
+
+    setDescription = (e) => {
+      this.setState({
+        newTaskDescription: e.target.value
+      })
+    }
+
 
     deleteThis = (e) =>{
       let id = e.target.id
       console.log(id)
       this.setState({
         tasks: this.state.tasks.filter(task =>{
-          console.log(task.id !== id)
-           return task.id !== id
+          console.log(task.id != id)
+           return task.id != id
          })
-    })
+      })
     }
 
-    addThis = () =>{
-      console.log('hi! i should add but...')
+    addThis = (e) =>{
+     
+      var newTask = {
+        id: this.state.tasks[this.state.tasks.length-1].id+1 && 0,
+        title: this.state.newTaskTitle,
+        description: this.state.newTaskDescription
+      }
+      this.state.tasks.push(newTask)
+      //this.state.tasks.push(newTask))
+      this.setState({
+        tasks: this.state.tasks
+      })
     }
 
 
@@ -50,8 +75,13 @@ class App extends Component {
           </Navbar>
           <p className="App-intro">
           <h2>Add new todo's</h2>
+
            <Row>
+           {
+                  console.log(this.state.tasks)
+                }
               {
+
                 this.state.tasks.map((elem)=>{
                   return(
                     <Card 
@@ -63,8 +93,21 @@ class App extends Component {
                     )
                 })
               }
-            <Button floating large className='blue' waves='light' icon='add' s={2} />
-            </Row>
+              </Row>
+              <Row>
+              <Modal
+                header='Add Task'
+                s={12}
+                actions={<Button onClick={this.addThis}> Add Task </Button> }
+                trigger={<Button floating large className='blue' waves='light' icon='add' s={2} />}>
+                  <Row>
+                    <Input s={12} placeholder='Write a description title' onChange={this.setTitle}><Icon>title</Icon></Input>
+                    <Input s={12} placeholder='Write a description about task' onChange={this.setDescription}><Icon>text_format</Icon></Input>
+                  </Row>
+              </Modal>
+              </Row>
+           {/*  />*/}
+            
           </p>
       </div>
     );
