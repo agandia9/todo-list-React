c67af482f6ad58b7594eefca66c4e7f8fcbfcc87
agandia9 <alejandro.gandia9@gmail.com>
2018-01-30 16:05:58 +0100

    adding tasks

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+
 import Card from './card/card.js'
-import { Button, Row, Navbar, NavItem, Col } from 'react-materialize'
+import { Button, Row, Navbar, NavItem, Col, Modal } from 'react-materialize'
 
 class App extends Component {
-    status={
+  constructor(props){
+    super()
+    this.state={
       tasks: [{
         id:'1',
         title: 'Lorem?',
@@ -15,45 +18,54 @@ class App extends Component {
       {
         id:'2',
         title: 'Lorem!',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        description: 'Maiores fugit veniam, dolore assumenda adipisicing elit.',
         date: '14/2'
       }]
     }
+  }
 
-    deleteThis = () =>{
-      console.log('hi! i should delete but...')
+    deleteThis = (e) =>{
+      let id = e.target.id
+      console.log(id)
+      this.setState({
+        tasks: this.state.tasks.filter(task =>{
+          console.log(task.id !== id)
+           return task.id !== id
+         })
+    })
     }
 
     addThis = () =>{
       console.log('hi! i should add but...')
     }
-  render() {
 
+
+  render() {
     return (
       <div className="App">
         <Navbar brand='logo' left>
           <NavItem href='get-started.html'>
             <img href={logo} />
-          </NavItem>
-        </Navbar>
-        <p className="App-intro">
-        <h2>Add new todo's</h2>
-         <Row>
-            {
-              this.status.tasks.map((elem)=>{
-                return(
-                  <Card 
-                      id={elem.id}
-                      title={elem.title}
-                      description={elem.description}
-                      deleteThis={this.deleteThis}
-                  />
-                  )
-              })
-            }
-            <Button floating large className='blue' waves='light' icon='add' s={2} onClick={this.addThis}/>
-          </Row>
-        </p>
+            </NavItem>
+          </Navbar>
+          <p className="App-intro">
+          <h2>Add new todo's</h2>
+           <Row>
+              {
+                this.state.tasks.map((elem)=>{
+                  return(
+                    <Card 
+                        id={elem.id}
+                        title={elem.title}
+                        description={elem.description}
+                        deleteThis={this.deleteThis}
+                    />
+                    )
+                })
+              }
+            <Button floating large className='blue' waves='light' icon='add' s={2} />
+            </Row>
+          </p>
       </div>
     );
   }
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Col, Card } from 'react-materialize'
 
-class App extends Component {
+class App extends Component {	
   render() {
     return (
 		<Col s={2} className='grid-example'>
-            <Card className='blue-grey darken-1' textClassName='white-text' title={this.props.title} actions={[<a href='#' onClick={this.props.deleteThis}>Delete task</a>]}>
+            <Card className='blue-grey darken-2' textClassName='white-text' title={this.props.title} actions={[<a href='#' id={this.props.id} onClick={this.props.deleteThis}>Delete task</a>]}>
             	{this.props.description}
             </Card>
 		</Col>
