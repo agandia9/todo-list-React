9038143a4d0198f92330caf8beda97fb098dd9fe
agandia9 <alejandro.gandia9@gmail.com>
2018-01-31 13:29:54 +0100

    edit and delete todos...

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,12 +3,11 @@ import { Col, Card } from 'react-materialize'
 import './card.css'
 
 class App extends Component {
-
   render() {
     return (
 		<Col s={6} m={4} l={2} className='grid-example'>
             <Card className='blue-grey darken-2' textClassName='white-text' title={this.props.title} actions={[<a href='#' id={this.props.id} onClick={this.props.deleteThis}>Delete task</a>]}>
-            	<textarea id={this.props.id} onChange={this.props.editThis} value={this.props.description}></textarea>
+            	<textarea className="edit" id={this.props.id} onChange={this.props.editThis} value={this.props.description} ></textarea>
             </Card>
 		</Col>
     );
